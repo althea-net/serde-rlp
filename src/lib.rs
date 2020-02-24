373bab222a8d53275d2b377034f2086786ff7464
Zaki Manian <zaki@manian.org>
2020-02-23 21:05:44 -0800

    Update edition

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate byteorder;
-extern crate num;
-extern crate serde;
 
 pub mod de;
 mod error;
